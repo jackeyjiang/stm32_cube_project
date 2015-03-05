5f5283727d2cc8afa1ad5d0eaa2633a162f05721
江冲 <jackeyjiang123@gmall.com>
2015-03-05 15:38:24 +0800

    remove the .o .crf

--- stm32_lwip/Projects/MDK-ARM/stm32_lwip Configuration/crc_16.d
+++
@@ -1,4 +0,0 @@
-stm32_lwip configuration\crc_16.o: ..\..\Task\crc_16.c
-stm32_lwip configuration\crc_16.o: ..\..\Task\crc_16.h
-stm32_lwip configuration\crc_16.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-stm32_lwip configuration\crc_16.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
--- stm32_lwip/Projects/MDK-ARM/stm32_lwip Configuration/ili9325.d
+++
@@ -1,4 +0,0 @@
-stm32_lwip configuration\ili9325.o: ..\..\Middlewares\STM32_EVAL\Components\ili9325\ili9325.c
-stm32_lwip configuration\ili9325.o: ..\..\Middlewares\STM32_EVAL\Components\ili9325\ili9325.h
-stm32_lwip configuration\ili9325.o: ..\..\Middlewares\STM32_EVAL\Components\ili9325\..\Common\lcd.h
-stm32_lwip configuration\ili9325.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
